c3829e9a473461dc0ca1432b70df21cab3b0ab90
iltonkp <iltonk.si@gmail.com>
2019-05-22 14:10:10 -0300

    GoReact - mod3 - 1 - configurando axios e redux-saga

--- src/strore/index.js
+++ src/strore/index.js
@@ -1,9 +1,21 @@
 import { createStore, compose, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
 import reducers from './reducers';
+import sagas from './sagas';
 
-const composer = process.env.NODE_ENV === 'development' ? compose(applyMiddleware(...[])) : applyMiddleware(...[]);
+const sagaMiddleware = createSagaMiddleware();
+const middlewares = [sagaMiddleware];
+
+const composer = process.env.NODE_ENV === 'development'
+  ? compose(
+    applyMiddleware(...middlewares),
+    // console.tron.createEnhancer(),
+  )
+  : applyMiddleware(...middlewares);
 
 const store = createStore(reducers, composer);
+
+sagaMiddleware.run(sagas);
 
 export default store;
