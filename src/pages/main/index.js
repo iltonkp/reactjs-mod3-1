1268fda4fcfbe0fc602fcc4b5d3bf3e62a6f9803
iltonkp <iltonk.si@gmail.com>
2019-05-23 11:17:07 -0300

    GoReact - mod3 - 1 - exibindo erro

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -16,6 +16,7 @@ class Main extends Component {
     addFavoriteRequest: PropTypes.func.isRequired,
     favorites: PropTypes.shape({
       loading: PropTypes.bool,
+      error: PropTypes.oneOfType([null, PropTypes.string]),
       data: PropTypes.arrayOf(
         PropTypes.shape({
           id: PropTypes.number,
@@ -53,6 +54,7 @@ class Main extends Component {
           <button type="submit">Adicionar</button>
 
           {favorites.loading && <span>Carregando...</span>}
+          {!!favorites.error && <span style={{ color: '#f00' }}>{favorites.error}</span>}
         </form>
 
         <ul>
