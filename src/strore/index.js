79882b92755e4dfeb8cc54d1f2a170f373b0a94e
iltonkp <iltonk.si@gmail.com>
2019-05-23 10:59:10 -0300

    GoReact - mod3 - 1 - plugin do saga no reactootron

--- src/strore/index.js
+++ src/strore/index.js
@@ -4,6 +4,8 @@ import createSagaMiddleware from 'redux-saga';
 import reducers from './reducers';
 import sagas from './sagas';
 
+// const sagaMonitor = process.env.NODE_ENV === 'development' ? console.tron.createSagaMonitor() : null;
+
 const sagaMiddleware = createSagaMiddleware();
 const middlewares = [sagaMiddleware];
 
